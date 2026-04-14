8c83f4039337139aca1fe111d3529ca173a4c555
Carlos-E-Souza <carlos.e.souza2653@gmail.com>
2026-04-14 18:51:16 -0300

    teste 7

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -11,6 +11,7 @@ TEST_CASE("Numeros romanos - algarismos unicos", "[romanos]") {
   REQUIRE(romanos_para_decimal("L") == 50);
   REQUIRE(romanos_para_decimal("C") == 100);
   REQUIRE(romanos_para_decimal("D") == 500);
+  REQUIRE(romanos_para_decimal("M") == 1000);
 }
 
 /*
