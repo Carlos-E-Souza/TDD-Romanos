5d38623d80670e8d29c5c49a4eeac77355c5b30a
Carlos-E-Souza <carlos.e.souza2653@gmail.com>
2026-04-14 19:00:29 -0300

    teste 17

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -27,5 +27,6 @@ TEST_CASE("Numeros romanos - subtracao basica", "[romanos]") {
   REQUIRE(romanos_para_decimal("IV") == 4);
   REQUIRE(romanos_para_decimal("IX") == 9);
   REQUIRE(romanos_para_decimal("XL") == 40);
+  REQUIRE(romanos_para_decimal("XC") == 90);
 }
 
