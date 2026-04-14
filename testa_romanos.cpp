3926cef94847f0bf69c984e4c1c726eff2525b5d
Carlos-E-Souza <carlos.e.souza2653@gmail.com>
2026-04-14 18:52:46 -0300

    teste 9

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -16,6 +16,7 @@ TEST_CASE("Numeros romanos - algarismos unicos", "[romanos]") {
 
 TEST_CASE("Numeros romanos - adicoes simples", "[romanos]") {
   REQUIRE(romanos_para_decimal("II") == 2);
+  REQUIRE(romanos_para_decimal("III") == 3);
 }
 
 /*
