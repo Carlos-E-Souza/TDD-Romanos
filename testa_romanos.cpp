6b496d620e9bf086477c5a44ca07044a578c7eaa
Carlos-E-Souza <carlos.e.souza2653@gmail.com>
2026-04-14 18:59:03 -0300

    teste 12

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -19,5 +19,6 @@ TEST_CASE("Numeros romanos - adicoes simples", "[romanos]") {
   REQUIRE(romanos_para_decimal("III") == 3);
   REQUIRE(romanos_para_decimal("XX") == 20);
   REQUIRE(romanos_para_decimal("VI") == 6);
+  REQUIRE(romanos_para_decimal("VII") == 7);
 }
 
